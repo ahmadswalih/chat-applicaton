0ef581581d73c028fbf1e6549ec33ba5e40f356e
ahmad swalih <ahmadswalih777@gmail.com>
2022-06-09 22:01:39 +0530

    Created authMiddleware in the middleware folder and added the functionalities

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -4,10 +4,11 @@ const {
   authUser,
   allUsers,
 } = require("../Controllers/userController");
+const { protect } = require("../Middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/login", authUser);
-router.route("/").post(registerUser).get(allUsers);
+router.route("/").post(registerUser).get(protect, allUsers);
 
 module.exports = router;

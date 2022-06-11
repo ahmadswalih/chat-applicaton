7f74c13d7157549522cbe61f4f9887be3eb14099
ahmad swalih <ahmadswalih777@gmail.com>
2022-06-11 22:30:35 +0530

    added navigation bar in the frontend , created component for search , Logo and profile. Created Logout Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
+import chatPage from "./Pages/chatPage";
 
 function App() {
   return (
     <div className="App">
       <Route path={"/"} component={HomePage} exact />
+      <Route path={"/chats"} component={chatPage} exact />
     </div>
   );
 }
---
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+  Tooltip,
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { ChatState } from "../../Context/chatProvider";
+import ProfileModal from "./ProfileModal";
+import { useHistory } from "react-router-dom";
+const SideDrawer = () => {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
+
+  const { user } = ChatState();
+  const history = useHistory();
+  const LogoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    history.push("/");
+  };
+
+  return (
+    <>
+      <Box
+        display={"flex"}
+        justifyContent="space-between"
+        alignItems={"center"}
+        bg="white"
+        w={"100%"}
+        p={"5px 10px 5px 10px"}
+        borderWidth={"5px"}
+      >
+        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+          <Button variant={"ghost"}>
+            <i className="fas fa-search" />
+            <Text display={{ base: "none", md: "flex" }} px="4">
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+        <Text fontSize={"2xl"} fontFamily={"Work sans"}>
+          Catch Up
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize={"2xl"} m={1} />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size={"sm"}
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModal>
+              <MenuDivider />
+              <MenuItem onClick={LogoutHandler()}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+    </>
+  );
+};
+
+export default SideDrawer;

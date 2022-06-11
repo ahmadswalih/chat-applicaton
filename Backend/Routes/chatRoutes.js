20e951ba01bedf1d71bbd9d3229f3bfdd3f8c62e
ahmad swalih <ahmadswalih777@gmail.com>
2022-06-11 12:22:50 +0530

    created getAllChat controller and route

--- Backend/Routes/chatRoutes.js
+++ Backend/Routes/chatRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { protect } = require("../Middlewares/authMiddleware");
-const { accessChat } = require("../Controllers/chatController");
+const { accessChat, fetchChat } = require("../Controllers/chatController");
 const router = express.Router();
 
-router.route("/").post(protect, accessChat); //.get(protect, fetchChats);
+router.route("/").post(protect, accessChat).get(protect, fetchChat);
 // router.route("/group").post(protect,createGroupChat)
 // router.route("/rename").put(protect,renameGroupChat)
 // router.route("/groupremove").put(protect,removeFromGroup)
